e0c3fb4371f9840ca6ee00922cdacad322b4d33c
greyhwndz <greyhwndz@gmail.com>
2019-05-04 14:11:41 +0800

    implemented author controller methods show, update, delete

--- lib/library_api_web/controllers/author_controller.ex
+++ lib/library_api_web/controllers/author_controller.ex
@@ -13,8 +13,33 @@ defmodule LibraryApiWeb.AuthorController do
     with {:ok, %Author{} = author} <- Library.create_author(author_params) do
       conn
       |> put_status(:created)
-      |> put_resp_header("location", Library.author_path(conn, :show, author))
+      |> put_resp_header("location", Routes.author_path(conn, :show, author))
       |> render("show.json-api", data: author)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    author = Library.get_author!(id)
+
+    conn
+    |> render("show.json-api", data: author)
+  end
+
+  def update(conn, %{ "id" => id, "data" => _data = %{ "type" => "authors", "attributes" => author_params }}) do
+    author = Library.get_author!(id)
+
+    with {:ok, %Author{} = author} <- Library.update_author(author, author_params) do
+      conn
+      |> render("show.json-api", data: author)
+    end
+  end
+
+  def delete(conn, %{ "id" => id }) do
+    author = Library.get_author!(id)
+
+    with {:ok, %Author{}} <- Library.delete_author(author) do
+      conn
+      |> send_resp(:no_content, "")
     end
   end
 end
