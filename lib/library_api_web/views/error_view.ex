b9f1c046af8692c6bcfa499f045a428cd32f7397
greyhwndz <greyhwndz@gmail.com>
2019-05-09 20:51:54 +0800

    Encountered some error...

--- lib/library_api_web/views/error_view.ex
+++ lib/library_api_web/views/error_view.ex
@@ -20,12 +20,6 @@ defmodule LibraryApiWeb.ErrorView do
     |> JaSerializer.ErrorSerializer.format()
   end
 
-  # If you want to customize a particular status code
-  # for a certain format, you may uncomment below.
-  # def render("500.json", _assigns) do
-  #   %{errors: %{detail: "Internal Server Error"}}
-  # end
-
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.json" becomes
   # "Not Found".
--- lib/library_api_web/views/session_view.ex
+++ lib/library_api_web/views/session_view.ex
@@ -2,6 +2,14 @@ defmodule LibraryApiWeb.SessionView do
   use LibraryApiWeb, :view
 
   def render("token.json", user) do
-    %{id: user.id, email: user.email, username: user.username}
+    data = %{id: user.id, email: user.email, username: user.username}
+
+    jwt = %{data: data, sub: user.id}
+    |> Joken.token
+    |> Joken.with_signer(Joken.hs512("SOMESECRETVALUE"))
+    |> Joken.sign
+    IO.inspect jwt
+
+    %{token: jwt.token}
   end
 end
